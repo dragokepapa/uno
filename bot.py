85fbefd41f7e9763e27e451c325ae16b140b6320
dragokepapa <eriyerox683@gmail.com>
2026-04-08 13:45:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,10 @@ from actions import *
 from game_manager import gm
 from internationalization import _
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 TOKEN = config.TOKEN
@@ -19,10 +22,12 @@ TOKEN = config.TOKEN
 if not TOKEN:
     raise ValueError("TOKEN Heroku Config Vars mein daalo!")
 
+
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
 
+    # Commands
     dp.add_handler(CommandHandler("start", start_game))
     dp.add_handler(CommandHandler("help", help_handler))
     dp.add_handler(CommandHandler("new", new_game))
@@ -36,35 +41,37 @@ def main():
     dp.add_handler(CommandHandler("close", close_lobby))
     dp.add_handler(CommandHandler("lang", select_language))
 
+    # Inline + buttons
     dp.add_handler(InlineQueryHandler(inline_query))
     dp.add_handler(ChosenInlineResultHandler(chosen_inline_result))
     dp.add_handler(CallbackQueryHandler(button))
 
+    # New members
     dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_player))
-    
-    # Important line - alag line mein likha hai taaki tilde safe rahe
+
+    # Text messages (non-commands)
     text_filter = Filters.text & ~Filters.command
     dp.add_handler(MessageHandler(text_filter, button))
 
-    # === HEROKU WEBHOOK ===
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 8443))
-        app_name = os.environ.get("HEROKU_APP_NAME")
+    # === HEROKU / LOCAL ===
+    port = int(os.environ.get("PORT", 8443))
+    app_name = os.environ.get("HEROKU_APP_NAME")
 
-        if app_name:
-            webhook_url = f"https://{app_name}.herokuapp.com/{TOKEN}"
-            updater.start_webhook(
-                listen="0.0.0.0",
-                port=port,
-                url_path=TOKEN,
-                webhook_url=webhook_url
-            )
-            logger.info("Webhook ON ho gaya!")
-        else:
-            updater.start_polling()
-            logger.info("Local mode")
+    if app_name:
+        webhook_url = f"https://{app_name}.herokuapp.com/{TOKEN}"
+        updater.start_webhook(
+            listen="0.0.0.0",
+            port=port,
+            url_path=TOKEN,
+            webhook_url=webhook_url
+        )
+        logger.info("Webhook ON ho gaya!")
+    else:
+        updater.start_polling()
+        logger.info("Local mode")
 
-        updater.idle()
+    updater.idle()
+
 
 if __name__ == '__main__':
     main()
